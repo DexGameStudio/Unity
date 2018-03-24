bc616cc6fc82db12e95729431cc5ab737474d7bb
Arnoldo Silva <analisi@gmail.com>
2018-03-23 23:13:30 -0300

    Curso Udemy DevesLopes

---
+++ LearnToCode/Assets/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string name = "Thanos" ;
+        string equippedWapon = "Infinity Gaunlet";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
