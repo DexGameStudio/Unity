1cfd03755b65fdb265fd4cad1204f917bb2b323b
Arnoldo Silva <analisi@gmail.com>
2018-04-10 19:56:43 -0300

    Player Movement

---
+++ Cross Shooter/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera gameCamera;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = Input.mousePosition;
+        Vector3 worldPosition = gameCamera.ScreenToWorldPoint(mousePosition);
+
+        this.transform.position = new Vector3(worldPosition.x, worldPosition.y, this.transform.position.z);
+
+
+
+	}
+}
