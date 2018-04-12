d3576349d703d67ed2f11966e2fb459691e4549a
Éricke <erickemaciel@gmail.com>
2018-04-12 00:20:35 -0300

    Initial Commit

--- Cross Shooter/Assets/PlayerController.cs
+++ Cross Shooter/Assets/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public Camera gameCamera;
+    public GameObject bulletPrefab;
+
+    public float shootingCooldown;
+    float shootingTimer;
 
 
 	// Use this for initialization
@@ -19,7 +23,37 @@ public class PlayerController : MonoBehaviour {
 
         this.transform.position = new Vector3(worldPosition.x, worldPosition.y, this.transform.position.z);
 
+        //timer count down para atribuir cooldown para os disparos
+        shootingTimer -= Time.deltaTime;
 
 
-	}
+        if (Input.GetMouseButtonDown(0) && shootingTimer <=0)
+        {
+            shootingTimer = shootingCooldown;
+            //array directions contendo as 4 direções
+            Vector2[] directions = new Vector2[]{
+                Vector2.up,
+                Vector2.down,
+                Vector2.left,
+                Vector2.right
+            };
+
+            // Laço no array directions
+            foreach (Vector2 direction in directions)
+            {
+                // Instancia um prefab da bala
+                GameObject bulletObject = Instantiate(bulletPrefab);
+                bulletObject.transform.position = this.transform.position;
+
+                // desloca o prefab da bala
+                Bullet bullet = bulletObject.GetComponent<Bullet>();
+                bullet.moveDirection = direction;
+            }
+
+
+
+
+
+        }
+    }
 }
